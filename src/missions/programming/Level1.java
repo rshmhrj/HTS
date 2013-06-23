aa7ac85810648ab62e9fbe586043665e264689cf
Rishi Maharaj <dudewheysmehemail@gmail.com>
2013-06-23 11:11:55 -0500

    Syncing everything after a long time

--- src/missions/programming/Level1.java
+++ src/missions/programming/Level1.java
@@ -2,22 +2,28 @@ package missions.programming;
 
 import java.util.Scanner;
 
-public class Level1 {
+public class Level1
+{
 
-	public static void main(String args[]){
-		System.out.println("HTS-Programming-Level1: This program will unscrable words using a wordlist.");
-		body();
-		System.out.println("End of Program.");
-	}
-	
-	public static void body(){
-		Scanner input = new Scanner(System.in).useDelimiter("Answer:");
-		
-		System.out.println("Please paste scrambled words.  Include all text from \'List of scrambled words\' to ...word9,word10)");
-		String words = input.next();
-		
-		System.out.println(words);
-		System.out.println("End of Body.");
-	}
-	
+   public static void main(String args[])
+   {
+      System.out
+            .println("HTS-Programming-Level1: This program will unscrable words using a wordlist.");
+      body();
+      System.out.println("End of Program.");
+   }
+
+
+   public static void body()
+   {
+      Scanner input = new Scanner(System.in).useDelimiter("Answer:");
+
+      System.out
+            .println("Please paste scrambled words.  Include all text from \'List of scrambled words\' to ...word9,word10)");
+      String words = input.next();
+
+      System.out.println(words);
+      System.out.println("End of Body.");
+   }
+
 } // end class Level1
--- test/missions/programming/Level1Test.java
+++ test/missions/programming/Level1Test.java
@@ -2,23 +2,16 @@ package missions.programming;
 
 import junit.framework.*;
 
-public class Level1Test extends TestCase {
+public class Level1Test extends TestCase
+{
 
-	public void test1(){
-		/*
-		 * 
-eslact
-kihtgn
-eph1nixo
-sladiyn
-ivctae
-efeofc
-ssiomin
-qyutial
-bdfcgea
-esaytf
-		 * 
-		 */
-		
-	}
+   public void test1()
+   {
+      /*
+       * 
+       * eslact kihtgn eph1nixo sladiyn ivctae efeofc ssiomin qyutial bdfcgea
+       * esaytf
+       */
+
+   }
 }
