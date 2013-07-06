79fa30b2c88202a2946e8f12608ecd6a49e8d12a
Rishi Maharaj <dudewheysmehemail@gmail.com>
2013-07-06 01:33:45 -0500

    closed inputs and ins + formatting changes

--- src/missions/programming/Level1.java
+++ src/missions/programming/Level1.java
@@ -3,27 +3,25 @@ package missions.programming;
 import java.util.Scanner;
 
 public class Level1
-{
+	{
 
-   public static void main(String args[])
-   {
-      System.out
-            .println("HTS-Programming-Level1: This program will unscrable words using a wordlist.");
-      body();
-      System.out.println("End of Program.");
-   }
+		public static void main( String args[] )
+			{
+			System.out.println( "HTS-Programming-Level1: This program will unscrable words using a wordlist." );
+			body();
+			System.out.println( "End of Program." );
+			}
 
+		public static void body()
+			{
+			@SuppressWarnings( "resource" )
+			Scanner input = new Scanner( System.in ).useDelimiter( "Answer:" );
 
-   public static void body()
-   {
-      Scanner input = new Scanner(System.in).useDelimiter("Answer:");
+			System.out.println( "Please paste scrambled words.  Include all text from \'List of scrambled words\' to ...word9,word10)" );
+			String words = input.next();
+			input.close();
+			System.out.println( words );
+			System.out.println( "End of Body." );
+			}
 
-      System.out
-            .println("Please paste scrambled words.  Include all text from \'List of scrambled words\' to ...word9,word10)");
-      String words = input.next();
-
-      System.out.println(words);
-      System.out.println("End of Body.");
-   }
-
-} // end class Level1
+	} // end class Level1
--- test/missions/programming/Level1Test.java
+++ test/missions/programming/Level1Test.java
@@ -3,15 +3,14 @@ package missions.programming;
 import junit.framework.*;
 
 public class Level1Test extends TestCase
-{
+	{
 
-   public void test1()
-   {
-      /*
-       * 
-       * eslact kihtgn eph1nixo sladiyn ivctae efeofc ssiomin qyutial bdfcgea
-       * esaytf
-       */
+		public void test1()
+			{
+			/*
+			 * eslact kihtgn eph1nixo sladiyn ivctae efeofc ssiomin qyutial
+			 * bdfcgea esaytf
+			 */
 
-   }
-}
+			}
+	}
